dea117ff4f75c4999d9c8b2ecc00e3e06f0cd53b
Arsen Tasha <arsentasha@yahoo.com>
2022-11-21 11:49:46 +0100

    new pic entered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'newA';
+export class AppComponent implements OnInit {
+  images = ['wing.jpg', 'elep.jpg','mald.jpg'];
+  currentImg = 0;
+  showImg = true;
+
+  ngOnInit (){
+    this.updateImg();
+  }
+
+  updateImg(){
+    setInterval(()=>{
+      this.currentImg++;
+      this.currentImg = this.currentImg % this.images.length;
+      this.showImg = false ;
+
+
+      setTimeout(() => {
+        this.showImg = true;
+      }, 100);
+    }, 4000)
+  }
 }
